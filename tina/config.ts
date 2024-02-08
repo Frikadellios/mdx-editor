72176d2f7b7f9062026d7979b819ba4ba94fbcda
Hrihorii Ilin <devopsick@pm.me>
2024-02-08 21:35:12 +0000

    +

---
+++ tina/config.ts
@@ -0,0 +1,50 @@
+import { defineConfig } from 'tinacms'
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || 'main'
+
+export default defineConfig({
+  branch,
+  clientId: 'cbadd7a3-e630-47b7-81c2-acc03ac61016', // Get this from tina.io
+  token: '5a07dcf311719429f72485167868bedf08fb456d', // Get this from tina.io
+
+  build: {
+    outputFolder: 'admin',
+    publicFolder: 'public',
+  },
+  media: {
+    tina: {
+      mediaRoot: '',
+      publicFolder: 'public',
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: 'docs',
+        label: 'Docs',
+        path: 'src/content/docs',
+        fields: [
+          {
+            type: 'string',
+            name: 'title',
+            label: 'Title',
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: 'string',
+            name: 'description',
+            label: 'Description',
+          },
+          {
+            type: 'rich-text',
+            name: 'body',
+            label: 'Body',
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+})
